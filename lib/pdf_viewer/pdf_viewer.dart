f4d71dec0619be475faa86cfbca2c3fbc07d51a3
uluhulu <Aa123456>
2022-03-18 14:33:58 +0400

    add fixes: - remove scrolling via mouth wheel - add zooming

--- lib/pdf_viewer/pdf_viewer.dart
+++ lib/pdf_viewer/pdf_viewer.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
@@ -11,17 +12,56 @@ class PdfViewerWidget extends StatefulWidget {
 }
 
 class _PdfViewerWidgetState extends State<PdfViewerWidget> {
+  late PdfViewerController _controller;
+  late double _dy;
+  late double _zoomLevel;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = PdfViewerController();
+    _controller.addListener(({property}) {
+      _dy = _controller.scrollOffset.dy;
+    });
+    _zoomLevel = _controller.zoomLevel;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey,
-        appBar: AppBar(
-          title: const Text('Pdf Viewer'),
-        ),
-        body: SfPdfViewer.network(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        title: const Text('Pdf Viewer'),
+      ),
+      body: Listener(
+        onPointerSignal: (pointerSignal) {
+          if (pointerSignal is PointerScrollEvent) {
+            setState(() {
+              ///prevent scrolling
+              _controller.jumpTo(
+                yOffset: _dy,
+              );
+
+              ///zoom file depending on scroll direction
+              var upScroll = pointerSignal.scrollDelta.direction > 0;
+              if (upScroll) {
+                _zoomLevel += 0.25;
+              } else {
+                if (_zoomLevel > 1) {
+                  _zoomLevel -= 0.25;
+                }
+              }
+              _controller.zoomLevel = _zoomLevel;
+            });
+          }
+        },
+        child: SfPdfViewer.network(
           widget.url,
           interactionMode: PdfInteractionMode.pan,
-        ));
+          controller: _controller,
+        ),
+      ),
+    );
   }
-
 }
